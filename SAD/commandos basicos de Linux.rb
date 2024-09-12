6f6f0d7e7880c13c23f9a34a6e9c4299df526a20
ethane66 <alexsandererwin@gmail.com>
2024-09-12 14:03:28 +0200

    Redes-Linux

---
+++ SAD/commandos basicos de Linux.rb
@@ -0,0 +1,21 @@
+1: commandos para manejo de archivos y directorios
+    cd: cambiar de directorio
+    ls: listar archivos y directorios
+    mkdir: crear directorios
+    touch: crear archivos
+    cp: copiar archivos y directorios
+    mv: mover archivos y directorios
+    rm: eliminar archivos y directorios
+    pwd: mostrar directorio actual
+    cat: mostrar contenido de un archivo
+    more: mostrar contenido de un archivo por partes
+
+2: commandos para actualizar/instalar paquetes en Linux CLI
+    sudo apt update: actualizar la lista de paquetes disponibles
+    sudo apt upgrade: actualizar los paquetes instalados
+    sudo apt install <nombre_paquete>: instalar un paquete específico
+    sudo apt remove <nombre_paquete>: desinstalar un paquete específico
+    sudo apt autoremove: eliminar paquetes no utilizados
+    sudo apt search <nombre_paquete>: buscar un paquete específico
+
+3: 
