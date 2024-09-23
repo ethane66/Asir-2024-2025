a65bc4492784cc3f20103b2d86acf0476591a5d0
ethane66 <alexsandererwin@gmail.com>
2024-09-23 13:21:28 +0200

    trabajo

---
+++ WEB/Tema 1/practica1.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <meta http-equiv="content-type" content="text/html;
+charset=utf-8">
+</head>
+
+<body>
+    <?php
+    $num1=9;
+    $num2=2;
+    $suma=$num1+$num2;
+    echo "La suma de $num1 y $num2 es $suma<br>";
+    ?>
+</body>
+
+</html>
