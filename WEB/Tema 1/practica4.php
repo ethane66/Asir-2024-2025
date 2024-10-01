f9647b362a3d20ba4ff18736c364a82eb460ae91
ethane66 <user@parrot.hive.palinuro.cloud>
2024-10-01 08:09:58 +0000

    Sync

--- WEB/Tema 1/practica4.php
+++ WEB/Tema 1/practica4.php
@@ -14,9 +14,11 @@ charset=utf-8'>
     echo "<br>";
     echo "La longitud es " . (2*PI*$radio);
     echo "<br>";
-    echo "El Area es " . (PI*$radio*$radio);
+    echo "El Area es " . (PI* pow($radio,2));
     echo "<br>";
-    echo "El volumen es " . (4/3 * PI * $radio * $radio * $radio);
+    echo "El volumen es " . (4/3 * PI * pow($radio,3)); 
+                                       //*pow es para elevar al cuadrado
+                                        //tambien se puede usar **2 
     ?>
 </body>
 
