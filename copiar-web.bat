dddf524c974f5c193c69ab8edbd34e13d4ead5f9
ethane66 <alexsandererwin@gmail.com>
2024-10-04 10:33:34 +0200

    Agregar script para copiar archivos de una carpeta a otra

---
+++ copiar-web.bat
@@ -0,0 +1,9 @@
+@echo off
+set source=C:\Users\alumnoManana.ASIR2PC06\Documents\UwAmp\www\curso
+set destination=C:\Users\alumnoManana.ASIR2PC06\Documents\Asir-2024-2025\WEB
+
+echo Copiando archivos de %source% a %destination%
+xcopy /s /y "%source%\*" "%destination%"
+
+echo Archivos copiados y reemplazados exitosamente.
+pause
