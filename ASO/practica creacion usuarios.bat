a6dec1cf86d43071bf1e9084b48d6422bf13a355
ethane66 <alexsandererwin@gmail.com>
2024-10-08 13:09:29 +0200

    Actualizar script de creación de usuarios en Active Directory

--- ASO/practica creacion usuarios.bat
+++ ASO/practica creacion usuarios.bat
@@ -55,40 +55,37 @@ dsadd computer "cn=dameq2,ou=dam,ou=primero,ou=equiposaulas,dc=valleeae,dc=local
 
 echo "Añadiendo los equipos de segundo"
 
-dsadd computer "cn=asireq1,ou=asir,ou=segundo,ou=equiposaulas,dc=valleeae,dc=local"
-dsadd computer "cn=asireq2,ou=asir,ou=segundo,ou=equiposaulas,dc=valleeae,dc=local"
-dsadd computer "cn=dameq1,ou=dam,ou=segundo,ou=equiposaulas,dc=valleeae,dc=local"
-dsadd computer "cn=dameq2,ou=dam,ou=segundo,ou=equiposaulas,dc=valleeae,dc=local"
+dsadd computer "cn=2asireq1,ou=asir,ou=segundo,ou=equiposaulas,dc=valleeae,dc=local"
+dsadd computer "cn=2asireq2,ou=asir,ou=segundo,ou=equiposaulas,dc=valleeae,dc=local"
+dsadd computer "cn=2dameq1,ou=dam,ou=segundo,ou=equiposaulas,dc=valleeae,dc=local"
+dsadd computer "cn=2dameq2,ou=dam,ou=segundo,ou=equiposaulas,dc=valleeae,dc=local"
 
 
 echo "estoy creando los usuarios"
 
-for /f "tokens=1,2,3,4 delims=; usebackq" %%i in (`type alumnos.txt`) do (
-if "%%l"=="1asir" (
-echo Creando usuario %%i %%j en ou 1asir
-dsadd user "cn=%%i %%j,ou=1asir,ou=aulas,dc=valleeae,dc=local" -pwd %%k
-)
-if "%%l"=="2asir" (
-echo Creando usuario %%i %%j en ou 2asir
-dsadd user "cn=%%i %%j,ou=2asir,ou=aulas,dc=valleeae,dc=local" -pwd %%k
-)
-if "%%l"=="1smr" (
-echo Creando usuario %%i %%j en ou 1smr
-dsadd user "cn=%%i %%j,ou=1smr,ou=aulas,dc=valleeae,dc=local" -pwd %%k
-)
-if "%%l"=="2smr" (
-echo Creando usuario %%i %%j en ou 2smr
-dsadd user "cn=%%i %%j,ou=2smr,ou=aulas,dc=valleeae,dc=local" -pwd %%k
-)
-if "%%l"=="1dam" (
-echo Creando usuario %%i %%j en ou 1dam
-dsadd user "cn=%%i %%j,ou=1dam,ou=aulas,dc=valleeae,dc=local" -pwd %%k
-)
-if "%%l"=="2dam" (
-echo Creando usuario %%i %%j en ou 2dam
-dsadd user "cn=%%i %%j,ou=2dam,ou=aulas,dc=valleeae,dc=local" -pwd %%k
-)
-)
+echo "creando usuarios en profesores"
 
+dsadd user "cn=Ana Herrero,ou=profesores,dc=valleeae,dc=local" -pwd P@ssw0rd
+dsadd user "cn=Nico,ou=profesores,dc=valleeae,dc=local" -pwd P@ssw0rd
+dsadd user "cn=Carmen Navarro,ou=profesores,dc=valleeae,dc=local" -pwd P@ssw0rd
 
+echo "creando usuarios en aulas"
 
+for /f "tokens=1,2,3,4,5,6,7 delims=; usebackq" %%a in (`type listanombres.txt`) do (
+if "%%a"=="C1ASIR" (
+echo Creando usuario %%e %%c %%d en ou 1asir
+dsadd user "cn=%%e %%c %%d,ou=asir,ou=primero,ou=alumnosaulas,dc=valleeae,dc=local" -pwd %%g
+)
+if "%%a"=="C2ASIR" (
+echo Creando usuario %%e %%c %%d en ou 2asir
+dsadd user "cn=%%e %%c %%d,ou=asir,ou=segundo,ou=alumnosaulas,dc=valleeae,dc=local" -pwd %%g
+)
+if "%%a"=="C1DAM" (
+echo Creando usuario %%e %%c %%d en ou 1dam
+dsadd user "cn=%%e %%c %%d,ou=dam,ou=primero,ou=alumnosaulas,dc=valleeae,dc=local" -pwd %%g
+)
+if "%%a"=="C2DAM" (
+echo Creando usuario %%e %%c %%d en ou 2dam
+dsadd user "cn=%%e %%c %%d,ou=dam,ou=segundo,ou=alumnosaulas,dc=valleeae,dc=local" -pwd %%g
+)
+)
