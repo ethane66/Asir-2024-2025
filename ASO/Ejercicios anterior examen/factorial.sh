1705279e08ef14f813e731a44079738de06e7d97
ethane66 <124718717+ethane66@users.noreply.github.com>
2025-03-05 11:28:02 +0100

    Update factorial.sh

--- ASO/Ejercicios anterior examen/factorial.sh
+++ ASO/Ejercicios anterior examen/factorial.sh
@@ -4,7 +4,7 @@
 read -p "Introduce un número entre 1 y 9: " num
 
 # Validar que el número esté entre 1 y 9
-if [[ $num -lt 1 || $num -gt 9 ]]; then
+if [[ $num -lt 1 -o $num -gt 9 ]]; then
     echo "El número introducido '$num' no es un valor correcto"
     exit 1
 fi
