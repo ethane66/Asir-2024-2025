d46f40a027166dd76170d7ae441e25e0a32b16af
ethane66 <alexsandererwin@gmail.com>
2025-03-06 10:09:22 +0100

    Mejorar la validación de entrada en el script de factorial y agregar manejo de errores

--- ASO/simulacro examen/factorial.sh
+++ ASO/simulacro examen/factorial.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+factorial=1
+
+
 read -p "Introduce un numero entre 1 y 9 para hacer el factorial " num
+
 
 if [ "$num" -lt 1 -o "$num" -gt 9 ]; then
     echo "$num se sale de los parametros"
@@ -8,7 +12,6 @@ if [ "$num" -lt 1 -o "$num" -gt 9 ]; then
     
 fi 
 
-factorial=1
 for (( i=$num; i>1; i-- )); do
     factorial=$(( factorial * i ))
 done
