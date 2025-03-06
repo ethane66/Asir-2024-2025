3d9cf438fb4451dd0330deb387ad695a9556b837
ethane66 <alexsandererwin@gmail.com>
2025-03-06 09:48:17 +0100

    Agregar scripts de factorial con validación de entrada y manejo de errores

---
+++ ASO/simulacro examen/factorial.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+read -p "Introduce un numero entre 1 y 9 para hacer el factorial " num
+
+if [ "$num" -lt 1 -o "$num" -gt 9 ]; then
+    echo "$num se sale de los parametros"
+    exit
+    
+fi 
+
+factorial=1
+for (( i=$num; i>1; i-- )); do
+    factorial=$(( factorial * i ))
+done
+echo "El factorial de $num es $factorial"
+
---
+++ ASO/simulacro examen/factorial2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ $# -eq 0 ];then
+    echo "Introduce un parametro correcto"
+    exit
+done
+fi
+
