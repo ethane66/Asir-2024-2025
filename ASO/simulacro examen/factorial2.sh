379f1cb6b02d8b4e651202c644206b185045894f
ethane66 <alexsandererwin@gmail.com>
2025-03-06 10:34:01 +0100

    Mejorar el script de factorial para validar parámetros y calcular el factorial de múltiples entradas

--- ASO/simulacro examen/factorial2.sh
+++ ASO/simulacro examen/factorial2.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-if [ $# -eq 0 ];then
-    echo "Introduce un parametro correcto"
+if [ $# -eq 0 ]; then
+    echo "Introduce un parametro"
     exit
-done
 fi
 
+while [ $# -gt 0 ]; do
+    if [ "$1" -lt 1 -o "$1" -gt 9 ];then 
+        echo "El parametro introducido no esta entre 1 y 9"
+    else
+        factorial=1
+            for (( $i=$1; i>1; i-- ))
+                factorial=$( $factorial * $i )
+            done
+            echo "El factorial de $1 es $factorial"
+        fi
+    shift
+done
